81a8bbcd8e435a5c0bd5e637865072a1353fef22
Wagner Ferreira Freitas <wagnerffreitas1973@gmail.com>
2025-03-06 10:35:32 -0300

    Criação do capitulo 21 até o 40 de Jeremias 06/03/25 10:35

--- script.js
+++ script.js
@@ -2120,7 +2120,7 @@ function loadSobre() {
             <h2>Sobre</h2>
             <p>O projeto "Bíblia Sagrada" tem como objetivo oferecer uma ferramenta online completa e acessível para leitura e estudo da Bíblia Sagrada, além de conteúdos complementares como Harpa Cristã, Hinário Batista, Dicionário Bíblico e Concordância.</p>
             <p>Estão disponíveis diferentes versões da Bíblia, incluindo a Almeida Revista e Atualizada (ARA), Almeida Atualizada (AA), Almeida Corrigida e Fiel (ACF), e outras.</p>
-            <p>O projeto está em desenvolvimento, estando somente os livros até Jeremias 21 completos; a opção SLIDE para a exibição dos versículos em um Datashow. Existe também a opção para BAIXAR alguns conteúdos para estudo teológico e por último a opção UTILIDADES com alguns tópicos, incluído uma lista de sites com cursos variados, sendo a grande maioria gratuitos, contudo estão sendo organizados.</p>
+            <p>O projeto está em desenvolvimento, estando somente os livros até Jeremias 40 completos; a opção SLIDE para a exibição dos versículos em um Datashow. Existe também a opção para BAIXAR alguns conteúdos para estudo teológico e por último a opção UTILIDADES com alguns tópicos, incluído uma lista de sites com cursos variados, sendo a grande maioria gratuitos, contudo estão sendo organizados.</p>
         `;
         content.appendChild(sobreContent); // Esta linha adiciona o texto da opção "Sobre"
     }
